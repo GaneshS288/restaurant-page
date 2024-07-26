75009ff905d33fe567ff8ea0ef7e763a84b8e226
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2024-07-26 11:08:41 +0530

    provide basic project structure and config

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry : {
+        index : './src/index.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: "Restaraunt Page",
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(jpg|jpeg|gif|png|svg)$/i,
+                type: 'asset/resource'
+            },
+        ]
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, './dist'),
+        clean: true,
+    }
+}
