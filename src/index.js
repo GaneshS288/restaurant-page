0f70011eed250902cc239b86e44deb24cf4e842a
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2024-07-29 13:10:24 +0530

    make function to render menu and allow dynamic content generation with buttons

--- src/index.js
+++ src/index.js
@@ -3,7 +3,29 @@ import CrownImg from './crown.jpg'
 import { renderHeader } from './renderHeader.js'
 import { renderFooter } from './renderFooter.js';
 import { renderHome } from './renderHome.js'
+import { renderMenu } from './renderMenu.js'
 
 renderHeader(CrownImg);
 renderFooter();
 renderHome();
+
+const homeButton = document.querySelector('.home');
+homeButton.addEventListener('click', () => {
+    const content = document.querySelector('.content');
+    (document.querySelectorAll('.content > *')).forEach((childnode) => {
+        content.removeChild(childnode);
+    })
+
+    renderHome();
+} )
+
+const menuButton = document.querySelector('.menu');
+
+menuButton.addEventListener('click', () => {
+    const content = document.querySelector('.content');
+    (document.querySelectorAll('.content > *')).forEach((childnode) => {
+        content.removeChild(childnode);
+    })
+
+    renderMenu();
+} )
---
+++ src/renderMenu.js
@@ -0,0 +1,18 @@
+export function renderMenu() {
+    const menuContainer = document.querySelector('.content');
+    const menuTitle = document.createElement('h2');
+    const shaiPaneer = document.createElement('p');
+    const maharajaThali = document.createElement('p');
+    const daalBatiChurma = document.createElement('p');
+
+    menuTitle.textContent = "Menu";
+    shaiPaneer.textContent = "Shai Paneer -> 100Rs"
+    maharajaThali.textContent = "Maharaja Thali -> 180Rs"
+    daalBatiChurma.textContent = "Daal Bati Churma -> 70Rs"
+    
+    menuContainer.append(menuTitle);
+    menuContainer.append(document.createElement('hr'));
+    menuContainer.append(shaiPaneer);
+    menuContainer.append(maharajaThali);
+    menuContainer.append(daalBatiChurma);
+}
