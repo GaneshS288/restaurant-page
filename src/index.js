4927a39df820ef851f6b196ac001f74333ac9eba
Ganesh <115623665+GaneshS288@users.noreply.github.com>
2024-07-29 11:26:22 +0530

    make function to render home contents

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './style.css'
 import CrownImg from './crown.jpg'
 import { renderHeader } from './renderHeader.js'
 import { renderFooter } from './renderFooter.js';
+import { renderHome } from './renderHome.js'
 
 renderHeader(CrownImg);
 renderFooter();
+renderHome();
---
+++ src/renderHome.js
@@ -0,0 +1,17 @@
+export function renderHome() {
+    const homeContainer = document.querySelector('.content');
+    const homeTitle = document.createElement('h2');
+    const homeLineBreak = document.createElement('br');
+    const homeParagraph = document.createElement('p');
+
+    homeTitle.textContent = "Home";
+    homeTitle.classList.add('title');
+
+    homeParagraph.textContent = "At Maharaja Culinary we aim to treat all our customers as kings and queens. All our staff is highly trained with many years of industry experience, be it for serving your food or making it. The recreated atmosphere of Rajasthan castles will make you blush from hapiness. Come and try out this exquisite experience, we promise that you will feel like royalty.";
+
+    homeParagraph.classList.add('home-content');
+
+    homeContainer.append(homeTitle);
+    homeContainer.append(homeLineBreak);
+    homeContainer.append(homeParagraph);
+}
